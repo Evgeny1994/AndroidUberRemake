7218d1fa969a8d8cec1cda4bf3e3ebcc8558ea60
Evgeny <myadelko94@mail.ru>
2023-02-27 21:32:25 +0300

    commit

---
+++ app/src/main/java/instagram/downloader/com/androiduberremake/Common.java
@@ -0,0 +1,9 @@
+package instagram.downloader.com.androiduberremake;
+
+/**
+ * Created by Евгений on 25.02.2023.
+ */
+
+public class Common {
+    public static final String DRIVER_INFO_REFERENCES = "DriverInfo";
+}
